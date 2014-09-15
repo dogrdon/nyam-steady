a8d1b3265a0d632a45e21510920e2c31f7455dc4
droquo <drew.s.gordon@gmail.com>
2014-09-15 15:10:14 -0400

    change mail address

--- app/mailers/use_mailer.rb
+++ app/mailers/use_mailer.rb
@@ -1,8 +1,8 @@
 class UseMailer < ActionMailer::Base
-  default(:from => "jronallo@gmail.com")
+  default(:from => "drew.s.gordon@gmail.com")
   def use_email(upload)
     @upload = upload
-    mail(:to => "jronallo@gmail.com", :subject => "[steady] #{@upload.name} #{@upload.organization}")
+    mail(:to => "drew.s.gordon@gmail.com", :subject => "[steady] #{@upload.name} #{@upload.organization}")
     attachments['upload.csv'] = @upload.csv.read if @upload.csv
   end
 end
